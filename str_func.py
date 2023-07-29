81ee8b5e82a503dbb7c521a62d92cecd68794357
Oleg Usachev <usaechv97@mail.ru>
2023-07-29 21:20:53 +0500

    ya zaputalsya

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def add(word):
+    """
+    YA HOCHU KUSHAT'
+    """
     return word.title()
 
+def add_upper(word):
+    """
+    Получает слово и выводит его
+    большими буквами
+    """
+    return word.upper()
